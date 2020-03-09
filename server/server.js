ad4a5bc4e2bc8bd5816207d0a0d98ddadfb82175
user <mattjackmcgregor@gmail.com>
2020-03-10 09:47:35 +1300

    setup production build and server

--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const express = require('express')
 const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
 
+const port = process.env.PORT || 3000
 app.use(express.static(publicPath))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
 })
 
-const port = 3000
+
 app.listen(port, () => {
   console.log(`server is runnin on port ${port}`)
 })
