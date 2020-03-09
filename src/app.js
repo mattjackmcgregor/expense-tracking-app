6d032a912d8fd14a5f832c7cdb5b164b628b8426
user <mattjackmcgregor@gmail.com>
2020-03-10 09:07:21 +1300

    optimising webpack for dev and production

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import AppRouter from './routes/AppRouter'
-import './styles/styles.scss'
-import 'normalize.css/normalize.css'
 import configureStore from './store/configureStore'
 import {addExpense, removeExpense, editExpense} from './actions/expenses'
 import {setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate} from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
+import './styles/styles.scss'
+import 'normalize.css/normalize.css'
 
 const store = configureStore()
-
+console.log('test')
 
 // store.dispatch(setTextFilter('water'))
 
 
-const jsx = (
+const jsx = ( 
   <Provider store={store}>
     <AppRouter />
   </Provider>
