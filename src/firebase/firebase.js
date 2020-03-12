871b3ca72a6daba5d2e19abc87e15f67fd6a471c
user <mattjackmcgregor@gmail.com>
2020-03-13 08:26:58 +1300

    added firebase to start of .env vars

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,22 @@ import * as firebase from 'firebase'
 
  // Your web app's Firebase configuration
  const firebaseConfig = {
-  apiKey: process.env.API,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
+  // apiKey: process.env.FIREBASE_API,
+  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.FIREBASE_DATABASE_URL,
+  // projectId: process.env.FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.FIREBASE_APP_ID,
+  // measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -48,14 +48,23 @@ module.exports = (env) => {
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
-        'process.env.API': JSON.stringify(process.env.API),
-        'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
-        'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
-        'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
-        'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
-        'process.env.MESSAGINE_SENDER_ID': JSON.stringify(process.env.MESSAGINE_SENDER_ID),
-        'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
-        'process.env.MEASUREMENT_ID': JSON.stringify(process.env.MEASUREMENT_ID),
+        // 'process.env.API': JSON.stringify(process.env.API),
+        // 'process.env.AUTH_DOMAIN': JSON.stringify(process.env.AUTH_DOMAIN),
+        // 'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+        // 'process.env.PROJECT_ID': JSON.stringify(process.env.PROJECT_ID),
+        // 'process.env.STORAGE_BUCKET': JSON.stringify(process.env.STORAGE_BUCKET),
+        // 'process.env.MESSAGINE_SENDER_ID': JSON.stringify(process.env.MESSAGINE_SENDER_ID),
+        // 'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
+        // 'process.env.MEASUREMENT_ID': JSON.stringify(process.env.MEASUREMENT_ID),
+
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
       })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
