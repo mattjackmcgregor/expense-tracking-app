1942e04ef15c1adb783c7c4909e092f052d2c516
user <mattjackmcgregor@gmail.com>
2020-03-24 15:08:25 +1300

    redirecting login/logout

--- src/app.js
+++ src/app.js
@@ -1,34 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import AppRouter from './routes/AppRouter';
-import configureStore from './store/configureStore';
-import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-import 'react-dates/lib/css/_datepicker.css';
-import { firebase } from './firebase/firebase';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import AppRouter, {history} from './routes/AppRouter'
+import configureStore from './store/configureStore'
+import { startSetExpenses } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+import 'react-dates/lib/css/_datepicker.css'
+import { firebase } from './firebase/firebase'
 
-const store = configureStore();
+
+const store = configureStore()
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
-);
+)
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+let hasRendered = false
+const renderApp = () => {
+  if(!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'))
+    hasRendered = true
+  }
+}
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-});
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    console.log('log in');
+    console.log('log in')
+    store.dispatch(startSetExpenses()).then(() => {
+    renderApp()
+    if (history.location.pathname==='/') {
+      history.push('/dashboard')
+    }
+    })
   } else {
-    console.log('log out');
+    renderApp()
+    console.log('log out')
+    history.push('/')
   }
-});
+})
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import {startLogout} from '../actions/auth'
 export const Header = ({startLogout}) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to='/' activeClassName='is-active' exact={true}>home </NavLink>
+    <NavLink to='/dashboard' activeClassName='is-active' exact={true}>home </NavLink>
     <NavLink to='/create' activeClassName='is-active'>create </NavLink>
     <NavLink to='edit' activeClassName='is-active'>edit </NavLink>
     <button onClick={startLogout}>logout</button>
