5ae2a31c9122a9fc5dff19c035ef839adf4f744c
user <mattjackmcgregor@gmail.com>
2020-03-27 06:58:47 +1300

    added styles

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -14,11 +14,19 @@ import numeral from 'numeral'
 
 //betterway descructuring
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
-  <div>
-  <Link to={`/edit/${id}`}><h4>{description}</h4></Link>
-    <p>
-    {numeral(amount/100).format('$0,0.00')} - created: {moment(createdAt).format('MMMM Do YYYY')}</p>
-  </div>
+  
+  <Link className='list-item' to={`/edit/${id}`}>
+    <div>
+      <h4 className='list-item__title'>{description}</h4>
+      <span className='list-item__date'>{moment(createdAt).format('MMMM Do YYYY')}</span>
+    </div>
+    <div>
+      <h3 className='list-item__amount'>{numeral(amount/100).format('$0,0.00')}</h3>
+    </div>
+  </Link>
+
+    
+  
 )
 
 export default (ExpenseListItem)
